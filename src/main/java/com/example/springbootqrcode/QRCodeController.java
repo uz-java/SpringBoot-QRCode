4d1f2c3cc820d397479eec153ca0e5303d178bf7
asliddin <asliddintojaliyev88@gmail.com>
2022-10-17 20:31:58 +0500

    commit QrCode

--- src/main/java/com/example/springbootqrcode/QRCodeController.java
+++ src/main/java/com/example/springbootqrcode/QRCodeController.java
@@ -1,9 +1,39 @@
 package com.example.springbootqrcode;
 
+import com.google.zxing.WriterException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.util.Base64;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 17/10/22 20:29 (Monday)
  * SpringBoot-QRCode/IntelliJ IDEA
  */
+@Controller
 public class QRCodeController {
+    @GetMapping("/")
+    public String getQRCode(Model model){
+        String pdp="https://pdp.uz";
+        String github="https://github.com/uz-java";
+
+        final String QR_CODE_IMAGE_PATH="./src/main/resources/static/img/QRCode.png";
+
+        byte[] image=new byte[0];
+        try {
+            image=QRCodeGenerator.getQRCodeImage(pdp,250,250);
+            QRCodeGenerator.generateQRCodeImage(github,250,250,QR_CODE_IMAGE_PATH);
+        }catch (WriterException | IOException e){
+            e.printStackTrace();
+        }
+
+        String qrcode = Base64.getEncoder().encodeToString(image);
+        model.addAttribute("pdp",pdp);
+        model.addAttribute("github",github);
+        model.addAttribute("qrcode",qrcode);
+        return "qrcode";
+    }
 }
--- src/main/java/com/example/springbootqrcode/QRCodeGenerator.java
+++ src/main/java/com/example/springbootqrcode/QRCodeGenerator.java
@@ -1,9 +1,37 @@
 package com.example.springbootqrcode;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 17/10/22 20:29 (Monday)
  * SpringBoot-QRCode/IntelliJ IDEA
  */
 public class QRCodeGenerator {
+    public static void generateQRCodeImage(String text,int width,int height,String filePath) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter=new QRCodeWriter();
+        BitMatrix bitMatrix=qrCodeWriter.encode(text, BarcodeFormat.QR_CODE,width,height);
+        Path path= FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix,"PNG",path);
+    }
+
+    public static byte[] getQRCodeImage(String text,int width,int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter=new QRCodeWriter();
+        BitMatrix bitMatrix=qrCodeWriter.encode(text,BarcodeFormat.QR_CODE,width,height);
+
+        ByteArrayOutputStream pngOutputStream=new ByteArrayOutputStream();
+        MatrixToImageConfig config=new MatrixToImageConfig(0xD2191902, 0xFFFFC041);
+        MatrixToImageWriter.writeToStream(bitMatrix,"PNG",pngOutputStream,config);
+        return pngOutputStream.toByteArray();
+    }
 }
