fb4d7bdcc58b70a8f7af2e5dd26f02cf98319758
asliddin <asliddintojaliyev88@gmail.com>
2022-10-17 20:29:48 +0500

    commit QrCode

---
+++ src/main/java/com/example/springbootqrcode/QRCodeController.java
@@ -0,0 +1,9 @@
+package com.example.springbootqrcode;
+
+/**
+ * @author "Tojaliyev Asliddin"
+ * @since 17/10/22 20:29 (Monday)
+ * SpringBoot-QRCode/IntelliJ IDEA
+ */
+public class QRCodeController {
+}
